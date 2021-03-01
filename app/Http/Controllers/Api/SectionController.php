6146ea856fade6d052d210f2c0b2183dc79af31d
sharif7761 <sharif7761@gmail.com>
2021-03-02 01:49:23 +0600

    section crud

---
+++ app/Http/Controllers/Api/SectionController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Model\Section;
+use Illuminate\Http\Request;
+
+class SectionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $section = Section::all();
+        return response()->json($section);
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+         $validateData = $request->validate([
+             'class_id' => 'required',
+             'section_name' => 'required'
+         ]);
+
+         $section = Section::create($request->all());
+         return response('Section created');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $section = Section::findorfail($id);
+        return response()->json($section);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $section = Section::findorfail($id);
+        $section->update($request->all());
+        return response('section update');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Section::where('id', $id)->delete();
+        return response('section deleted');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,3 +2,4 @@
 
 Route::ApiResource('/class', 'Api\SclassController');
 Route::ApiResource('/course', 'Api\CourseController');
+Route::ApiResource('/section', 'Api\SectionController');
