2af3456a0aaf0a86e659fc431a9fa63ead90a065
sharif7761 <sharif7761@gmail.com>
2021-03-07 22:55:10 +0600

    jwt token login method

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,17 @@ Route::ApiResource('/class', 'Api\SclassController');
 Route::ApiResource('/course', 'Api\CourseController');
 Route::ApiResource('/section', 'Api\SectionController');
 Route::ApiResource('/student', 'Api\StudentController');
+
+
+Route::group([
+
+    'prefix' => 'auth'
+
+], function () {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
